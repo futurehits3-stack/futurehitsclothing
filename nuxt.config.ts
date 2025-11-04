8a07a0b7dd40855aad162109ec4ad161e0f005c1
Donte Trumble <futurehitsdonte@aol.com>
2025-11-04 13:54:00 -0500

    diabled ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false,
   build: {
     transpile: ['vuetify'],
   },
